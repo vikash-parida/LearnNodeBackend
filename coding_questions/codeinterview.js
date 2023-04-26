f24aff6157ade567e94770206ac968ae541bc638
vikash-parida <vikashparida067@gmail.com>
2023-04-26 11:00:07 +0530

    hof in js

---
+++ coding_questions/codeinterview.js
@@ -0,0 +1,28 @@
+// Q1
+// let a = [];
+// let b = [];   // ans is false memory location is different a and b 
+// console.log(a == b);
+// console.log(a === b);
+
+//Q2
+// let a = [];
+// let b = a   // ans is true memory location is same a and b 
+// console.log(a == b);
+// console.log(a === b);
+
+// Q3
+// let a = [20];
+// let b = [20];   // ans is true  a and b value is  same and datatype is also same
+// console.log(a[0] == b[0]);
+// console.log(a[0] === b[0]);
+
+// // Q4
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let obj = {fname:'vikash'}  
+// console.log(...arr);
+//              // this code output is a give in a string 1 2 3 4 5 6 7 8 9
+
+
+
+            
+             
---
+++ javascript/higherorderfun.js
@@ -0,0 +1,32 @@
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+
+// let squareArr = [];
+
+
+// for(let i = 0; i< arr.length;i++){
+//     squareArr.push(arr[i] * arr[i])
+// }
+
+// console.log(squareArr);
+
+// map method 
+// map will loop every element of 
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+
+// let squareArr = arr.map((num)=>{
+//  return num * num
+// })
+
+// console.log(squareArr);
+
+// function revArr(...args){
+//     const [first,second] = args
+
+//     console.log(first);
+//     console.log(second);
+// }
+
+// revArr("vikash","parida");
