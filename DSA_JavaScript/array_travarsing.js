13ecb687665fa360fef236481da957d3bd5610b7
vikash-parida <vikashparida067@gmail.com>
2023-05-07 12:11:20 +0530

    dsa array traversing and accessing  array element

---
+++ DSA_JavaScript/array_travarsing.js
@@ -0,0 +1,21 @@
+//   Array Traversing in javascript
+
+let Data = [1,2,3,4,5,6,7,8,9,10,77,85,25,91,15,45,96,36,45,78,12,65,94,100];
+
+// array traversing using for loop 
+// for(let i=0; i < Data.length;i++){
+//     console.log(`Array ${i} is ${Data[i]}`)
+// }
+
+
+// array traversing using wile loop
+// let j = 0;
+// let arrayLength = Data.length
+// while(j < arrayLength){
+//     console.log(`Array ${j} is ${Data[j]}`)
+//     j++
+// }
+
+// accessing array element using index number 
+
+console.log(Data[0]);
