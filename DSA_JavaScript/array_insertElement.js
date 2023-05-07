a3f1401032f667825954d14c49a5b3910f790456
vikash-parida <vikashparida067@gmail.com>
2023-05-07 13:52:16 +0530

    add element any index using splice method in js

--- DSA_JavaScript/array_insertElement.js
+++ DSA_JavaScript/array_insertElement.js
@@ -1,20 +1,28 @@
 // insert element in a array in any position 
 
 
-let newArrayValue = [30,60,80,10,90]; // array
+let newArrayValue = [30,60,80,10,90]; 
 
-let newEle  = 20;
+// array in ele insert using loop 
 
-let positionIndex = 1;
+// let newEle  = 20;
 
-for(let i = newArrayValue.length-1 ;i >= 0 ;i--){  //  loop start with last index value move next index
+// let positionIndex = 1;
 
-    if(i>= positionIndex){
-        newArrayValue[i+1] = newArrayValue[i];
-        if(i == positionIndex){
-            newArrayValue[i] = newEle;
-        }
-    }
-}
+// for(let i = newArrayValue.length-1 ;i >= 0 ;i--){  //  loop start with last index value move next index
+
+//     if(i>= positionIndex){
+//         newArrayValue[i+1] = newArrayValue[i];
+//         if(i == positionIndex){
+//             newArrayValue[i] = newEle;
+//         }
+//     }
+// }
+
+// array in ele insert using splice method in js
+
+newArrayValue.splice(2,0,2000);
+
+
 
 console.log(newArrayValue);
