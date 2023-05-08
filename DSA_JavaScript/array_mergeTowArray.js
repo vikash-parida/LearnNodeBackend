901af647e568ea46baa45756178e18448ed6efab
vikash-parida <vikashparida067@gmail.com>
2023-05-08 23:47:30 +0530

    in single for  key word using two loop and more use

--- DSA_JavaScript/array_mergeTowArray.js
+++ DSA_JavaScript/array_mergeTowArray.js
@@ -1,15 +1,28 @@
-let data1 = [1,2,3,4,5,6];
-let data2 = [7,8,9,10,11,12,13,14,15];
-let data3 = [];
+// let data1 = [1,2,3,4,5,6];
+// let data2 = [7,8,9,10,11,12,13,14,15];
+// let data3 = [];
 
 
-for (let i = 0; i < data1.length; i++) {
-  data3[i] = data1[i]
-}
+// for (let i = 0; i < data1.length; i++) {
+//   data3[i] = data1[i]
+// }
 
-for (let i = 0; i < data2.length; i++) {
-   data3[data3.length + i]  = data2[i]
+// for (let i = 0; i < data2.length; i++) {
+//    data3[data1.length + i]  = data2[i]
     
-}
+// }
 
-console.log(data3);
+// console.log(data3);
+
+
+// for loop using two or more loop using in single for key word
+
+// let arr = [10,20,30,40,50,60,70,80,90,100];
+// let arr1 = [11,22,33,44,55,66,77,88,99,111];
+
+
+// for(let i=0,j=0;i<arr.length,j<arr1.length;i++,j++){
+// console.log(`arr = ${arr[i]} and arr1 = ${arr1[j]}`);
+// }
+
+
