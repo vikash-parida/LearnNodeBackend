403f1efed0ec709d40a2a0712785a6f4193aaee2
vikash-parida <vikashparida067@gmail.com>
2023-05-11 23:25:57 +0530

    bind method in js

---
+++ javascript/JavaScriptFD/callApplyBind.js
@@ -0,0 +1,23 @@
+// // call apply bind
+
+// let obj = {
+//     name: 'zhangsan',
+//     sayName(age){
+//         console.log(this.name + "  age is " + age)
+//     }
+// }
+
+
+// let obj1 =  {
+//     name: 'lisi',
+// }
+
+// obj.sayName.call(obj1,25)
+
+// function multiply(a,b){
+// return a*b;
+// }
+
+// let multiplyByTwo = multiply.bind(this,2);
+
+
