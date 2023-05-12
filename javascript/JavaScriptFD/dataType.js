27092687369a6afd2d362eb0610d76e0fe287c44
vikash-parida <vikashparida067@gmail.com>
2023-05-12 10:21:37 +0530

    datatype in javascript

---
+++ javascript/JavaScriptFD/dataType.js
@@ -0,0 +1,21 @@
+//primitive dataType 
+
+// console.log(typeof 2);
+// console.log(typeof "string");
+// console.log(typeof true);
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof Symbol("cdce"));
+
+// //Non-primitive dataType
+
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(typeof function(){});
+
+// // raperObject
+// let num1 = 1000
+// console.log(num1.toString())
+
+
+
