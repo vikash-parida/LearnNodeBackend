b3c0f745db9be8eb8fc1d77ccd47ecb4337d5e55
vikash-parida <vikashparida067@gmail.com>
2023-05-17 16:01:48 +0530

    HOD function in js  and first class function js

---
+++ javascript/JavaScriptFD/higherOrderFun.js
@@ -0,0 +1,77 @@
+// // function logAachal(){                     //normal function in javascript
+// //    let arr
+// //     for (let i = 0; i < 1000000000; i++) {
+// //         const arr = i;
+        
+// //     }
+
+// //     return `aachal login successfully`
+// // }
+
+// // function logNeha(){
+// //     let arr
+// //      for (let i = 0; i < 10000000000; i++) {
+// //          const arr = i;
+         
+// //      }
+ 
+// //      return `Neha login successfully`
+// //  }
+ 
+// // console.log(logNeha());
+// // console.log(logAachal());
+
+// // HOD function in javascript
+// function giveMessage(user){
+//     return `${user} login successfully`
+// }
+
+// function auth(verify){
+//     let result = []
+//     for (let i = 0; i < verify; i++) {
+//        result.push = i
+//     }
+// }
+
+// function login(persion,fun){
+//    if(persion.level === 'admin'){
+//     fun(500000000)
+//    }
+//    if(persion.level === 'user'){
+//     fun(1000000)
+//    }
+//   return giveMessage(persion.name)
+// }
+
+
+// console.log(login({name:"vikash",level:"user"},auth));
+
+
+
+
+
+
+
+
+// create multiply function in js using HOD function
+
+
+function multiplyBy(num1){
+
+    return function(num2){
+        return num1*num2;
+    }
+
+}
+
+// let multiplyByOne = multiplyBy(1);  multiply of one function 
+// let multiplyByTwo = multiplyBy(2);  multiply of two function
+
+
+
+console.log(multiplyByOne(1));
+console.log(multiplyByOne(2));
+console.log(multiplyByOne(3));
+
+console.log(multiplyByTwo(2));
+console.log(multiplyByTwo(3));
