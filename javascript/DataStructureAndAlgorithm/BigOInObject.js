aedf20684cae8881d8687d0facdfbaa6b22fff2f
vikash-parida <vikashparida067@gmail.com>
2023-05-18 22:53:13 +0530

    BigO time complicity of object

---
+++ javascript/DataStructureAndAlgorithm/BigOInObject.js
@@ -0,0 +1,30 @@
+// BigO time complicity of object
+// inserting in object is BigO - > O(1)
+// remove in object is BigO -> O(1);
+// searching in object is BigO  -> O(N);
+//accessing in object is BigO -> O(1);
+
+
+
+
+// let instructor = {
+//     firstName:"vikash parida",
+//     isInstructor: true,
+//     favoriteNumber:[1,2,3,4,5,6,7,8,9,0]
+// }
+
+// console.log(Object.keys(instructor));  // BigO ->  O(N)
+// console.log(Object.entries(instructor)); // BigO -> O(N);
+// console.log(Object.values(instructor));  // BigO -> O(N)
+// console.log(instructor.hasOwnProperty("firstName")) BIgO -> O(1)
+
+
+
+
+// for (const key in instructor) {
+//     // if (Object.hasOwnProperty.call(object, key)) {
+//         const element = instructor[key];
+        
+//     // }
+//    console.log(element);
+// }
