ac5156715b282442f203711005642ca7341febce
vikash-parida <vikashparida067@gmail.com>
2023-05-19 11:39:37 +0530

    find second  largest and smallest number

---
+++ javascript/DataStructureAndAlgorithm/findSecondAndSamllest.js
@@ -0,0 +1,50 @@
+// let arr = [1,2,9,4,6,8,5,5,6,7,3]
+
+
+// function sortDesc(){
+//     let temp = false
+//  for (let i = 0; i < arr.length; i++) {
+    
+//     for (let j = i+1 ; j < arr.length; j++) {
+        
+//         if(arr[i] < arr[j]){
+
+//             temp = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = temp;
+//         }
+        
+//     }
+    
+//  }
+// }
+
+// sortDesc(arr)
+
+// console.log('second largest number is ',arr[1]);
+
+
+// let arr = [1,2,9,4,6,8,5,5,6,7,3]
+
+
+// function sortDesc(){
+//     let temp = false
+//  for (let i = 0; i < arr.length; i++) {
+    
+//     for (let j = i+1 ; j < arr.length; j++) {
+        
+//         if(arr[i] >  arr[j]){
+
+//             temp = arr[j];
+//            arr[j] = arr[i];
+//            arr[i] = temp;
+//         }
+        
+//     }
+    
+//  }
+// }
+
+// sortDesc(arr)
+
+// console.log('second smallest number is ',arr[1]);
