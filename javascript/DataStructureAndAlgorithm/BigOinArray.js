7d6c828d021bba0edeada2c15b7e73d9f6edd8db
vikash-parida <vikashparida067@gmail.com>
2023-05-21 09:53:20 +0530

    swap alternate even number

---
+++ javascript/DataStructureAndAlgorithm/BigOinArray.js
@@ -0,0 +1,27 @@
+// BigO time complicity of Array
+// inserting in object is BigO - > it depends  ->  inserting  element in end is BigO O(1) || inserting element beginning  of the  array  is BigO O(n)
+// remove in object is BigO -> it depends -> remove element in end of array is  BigO O(1)  || remove element beginning  of the array is BigO O(n)
+// searching in object is BigO  -> O(N);
+//accessing in object is BigO -> O(1);
+
+
+
+let name = ["vikash","Aachal","neha","ranita","sakshi"]
+
+//           "vikash" Aachal  neha  ranita  sakshi
+//              0      1       2      3       4
+
+
+//  Array built in method /BigO time complicity 
+
+// 1 Push      BigO O(1)
+// 2 pop       BigO O(1)
+// 3 shift     BigO O(N)
+// 4 unShift   BigO O(N)
+// 5 splice    BigO O(N)
+// 6 slice     BigO O(N)
+// 7 concat    BigO O(N)
+// 8 sort      BigO O(N*log N)
+// forEach/map/reduce/filter/etc  BigO O(N)
+
+
